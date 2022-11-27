5647b84f2482d9115b8d175af179bf91b62a3609
dikky setiwan <setyawandicky88@gmail.com>
2022-11-27 13:02:41 +0700

    update

--- app.js
+++ app.js
@@ -24,17 +24,23 @@ client.subscribe(topic)
 
 /* get received message */
 client.on('message', async (topic, message) => {
-    console.log(topic)
-    console.log(message.toString())
+    // console.log(topic)
+    // console.log(message.toString())
+    let data = JSON.parse(message.toString())
+    const url = "http://localhost:8000/api"
+    let body = {
+        temperature: data.temperature,
+        humidity: data.humidity,
+        soil_moisture: data.soil_moisture,
+        id_wilayah: data.id_wilayah
+    }
     try {
-        if(topic == "sensor/1/PH") {
+        if(topic == "sensor/1") {
             const PH = message.toString()
             const response = await axios({
-                method: 'get',
-                url: url + "/" + "PH",
-                data: {
-                    PH
-                }
+                method: 'post',
+                url: `${url}/sensor`,
+                data: body
             })
             console.log(response.data)
         }
